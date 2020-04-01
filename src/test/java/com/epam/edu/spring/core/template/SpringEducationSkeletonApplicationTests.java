7e32e043cfaa12f9a8950b0e30b0181ca4262c93
Egor <egot.dubovczcev@yandex.ru>
2020-04-01 10:20:10 +0400

    Добавил тесты

--- src/test/java/com/epam/edu/spring/core/template/SpringEducationSkeletonApplicationTests.java
+++ src/test/java/com/epam/edu/spring/core/template/SpringEducationSkeletonApplicationTests.java
@@ -1,17 +1,34 @@
-//package com.epam.edu.spring.core.template;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class SpringEducationSkeletonApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//		assertTrue(true);
-//	}
-//
-//}
+package com.epam.edu.spring.core.template;
+
+import com.epam.edu.spring.core.template.configuration.ColorFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertTrue;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringCoreTemplate.class)
+public class SpringEducationSkeletonApplicationTests {
+
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+    @Test
+    public void colorFactoryTest(){
+        ColorFactory colorFactory = new ColorFactory();
+        colorFactory.initColors();
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("yellow");
+        colors.add("black");
+        colors.add("purple");
+        assertTrue(colors.equals(colorFactory.getColors()));
+    }
+}
