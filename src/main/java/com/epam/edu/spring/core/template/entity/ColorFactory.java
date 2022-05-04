bd5c8d5a05915be309bc9e290c995aa3a372ace3
serg1971ch12@gmail.com <https://gitlab.skillbox.ru>
2022-05-05 01:37:53 +0500

    first excise about Spring

---
+++ src/main/java/com/epam/edu/spring/core/template/entity/ColorFactory.java
@@ -0,0 +1,13 @@
+package com.epam.edu.spring.core.template.entity;
+
+public class ColorFactory {
+    Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
